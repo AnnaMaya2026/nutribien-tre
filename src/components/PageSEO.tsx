71b811ba1ddf47c5ae75ec1694fe6dd72bd08aa4
AnnaMaya2026 <abboost13@gmail.com>
2026-06-07 16:18:22 +0200

    Update PageSEO.tsx

--- src/components/PageSEO.tsx
+++ src/components/PageSEO.tsx
@@ -1,26 +1,9 @@
-import { Helmet } from "react-helmet-async";
-
 interface PageSEOProps {
   title: string;
   description: string;
   path: string;
 }
 
-const SITE_URL = "https://nutrimeno.com";
-
-export default function PageSEO({ title, description, path }: PageSEOProps) {
-  const url = `${SITE_URL}${path}`;
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="canonical" href={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-    </Helmet>
-  );
+export default function PageSEO({ title }: PageSEOProps) {
+  return null;
 }
