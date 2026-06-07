80ed5b620a998b451baf711760da4490985d4d86
AnnaMaya2026 <abboost13@gmail.com>
2026-06-07 15:27:31 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
 
-// Register service worker for routine reminders / push notifications
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js").catch((err) => {
